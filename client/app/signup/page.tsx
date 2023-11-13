51aa9348d345e3cc90c8debb26cf4cba245253bd
Mohamed-Elemam <mohamed.elemam.younis@gmail.com>
2023-11-13 23:38:48 +0200

    add login function

--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -1,19 +1,22 @@
 "use client";
+import axios from "axios";
 import { ErrorMessage, Field, Form, Formik, useFormikContext } from "formik";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import toast,{ Toaster } from "react-hot-toast";
 import * as yup from "yup";
 
 interface FormValues {
   userName: string;
   email: string;
   password: string;
-  cpassword: string;
+  cPassword: string;
 }
 const initialValues: FormValues = {
   userName: "",
   email: "",
   password: "",
-  cpassword: "",
+  cPassword: "",
 };
 
 // yup validation schhema
@@ -29,15 +32,15 @@ const signupSchema = yup.object({
     .required("password is required")
     .min(8, "Password is too short - should be 8 chars minimum.")
     .matches(/[a-z]/, "Password can only contain Latin letters."),
-  cpassword: yup
+  cPassword: yup
     .string()
     .oneOf([yup.ref("password")], "password must match")
     .required("This field is required"),
 });
-//TODO trim email value
+
+
 const SignupButton = () => {
   const formik = useFormikContext();
-  console.log(formik);
   return (
     <div className="flex justify-center">
       <button
@@ -51,28 +54,47 @@ const SignupButton = () => {
   );
 };
 
+//TODO trim email value/**************************** */
 export default function Signup() {
+const router = useRouter()
+
+const handleSignup =async (values:FormValues)=>{
+  try{const {data}= await axios({
+    method: "post",
+    url: "http://localhost:8080/user/signUp",
+    data: values}
+   )
+   toast.success(data?.message)
+  //  !****************
+   router.push('/notes')
+   
+  }catch(error:any){
+    toast.error(error.response.data.message)
+   }
+  }
+
   return (
-    <>
+    <section className="container mx-auto">
+      <Toaster position="top-center"/>
+      <div className="text-center text-xl font-semibold">Sign up</div>
       <Formik
         initialValues={initialValues}
-        onSubmit={(values, actions) => {
-          console.log(values, actions);
-          alert(JSON.stringify(values));
+        onSubmit={(values) => {
+          handleSignup(values)
         }}
         validationSchema={signupSchema}
       >
-        {/* ! */}
         <Form className="mt-20 p-10 rounded-lg  glass sm:w-[50%] xs:w-[80%] mx-auto">
           <div className="mb-6">
-            <label className="block dark:text-white font-semibold mb-2 ">
+            <label className="block dark:text-white  mb-3 ">
               Name
+              </label>
               <Field
                 required
                 type="text"
                 name="userName"
                 className="block rounded-lg border border-gray-300 
-              bg-gray-50 text-sm outline-none text-gray-900  ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              bg-gray-50  outline-none text-gray-900  ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Enter your name"
               />
               <ErrorMessage
@@ -80,19 +102,18 @@ export default function Signup() {
                 component="div"
                 className="text-red-500"
               />
-            </label>
           </div>
 
-          {/* <!-- remove name from label + className text sm --> */}
           <div className="mb-6">
-            <label className="block dark:text-white font-semibold mb-2">
+            <label className="block dark:text-white  mb-3">
               Email
+              </label>
               <Field
                 required
                 type="email"
                 name="email"
                 className="block rounded-lg border border-gray-300 
-              bg-gray-50 text-base outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              bg-gray-50  outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Enter your email address"
               />
               <ErrorMessage
@@ -100,19 +121,19 @@ export default function Signup() {
                 component="div"
                 className="text-red-500"
               />
-            </label>
             {/* {props.touched.email&&props.errors.email&&<p className="text-red-500">{errors.email}</p>} */}
           </div>
 
           <div className="mb-6">
-            <label className="block dark:text-white font-semibold mb-2">
+            <label className="block dark:text-white  mb-3">
               Password
+              </label>
               <Field
                 required
                 name="password"
                 type="password"
                 className="block rounded-lg border border-gray-300 
-    bg-gray-50 text-sm outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    bg-gray-50  outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Enter your password"
               />
               <ErrorMessage
@@ -120,28 +141,26 @@ export default function Signup() {
                 component="div"
                 className="text-red-500"
               />
-            </label>
           </div>
 
           <div className="mb-6">
-            <label className="block dark:text-white font-semibold mb-2">
+            <label className="block dark:text-white  mb-3">
               Confirm password
+              </label>
               <Field
                 required
-                name="cpassword"
+                name="cPassword"
                 type="password"
                 className="block rounded-lg border border-gray-300 
-              bg-gray-50 text-sm outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              bg-gray-50  outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Enter your password"
               />
               <ErrorMessage
-                name="cpassword"
+                name="cPassword"
                 component="div"
                 className="text-red-500"
               />
-            </label>
 
-            {/* {Formik.t} */}
           </div>
           <p className="my-3 dark:text-white">
             Do you have already an account ?
@@ -153,7 +172,7 @@ export default function Signup() {
           <SignupButton />
         </Form>
       </Formik>
-    </>
+    </section>
   );
 }
 
