9c9afa88c2829ca9c5a2534603169087a1d65b23
Mohamed Elemam <mohamed.elemam.younis@gmail.com>
2023-11-27 23:24:43 +0200

    modify ui

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -32,7 +32,7 @@ const LoginButton = () => {
       <button
         disabled={!(formik.isValid && formik.dirty)}
         type="submit"
-        className="disabled:opacity-50 disabled:cursor-not-allowed p-2.5 bg-blue-500 rounded-2xl w-[50%] text-center inline-block text-white hover:bg-blue-600 focus:ring ring-blue-300"
+        className="w-full disabled:opacity-50 disabled:cursor-not-allowed block rounded-lg bg-indigo-800 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-700 focus-visible:ring active:bg-indigo-600 md:text-base"
       >
         Login
       </button>
@@ -45,35 +45,32 @@ const demoLoginData: FormValues = {
   password: process.env.NEXT_PUBLIC_DEMO_PASSWORD as string,
 };
 
+export default function Home() {
+  const router = useRouter();
 
-export default function Home()
- {
-const router = useRouter()
+  const handleLogin = async (values: FormValues) => {
+    try {
+      const { data } = await axios({
+        method: "post",
+        url: "http://localhost:8080/user/login",
+        data: values,
+      });
+      toast.success(data?.message);
+      window?.localStorage?.setItem("userToken", data.userToken);
+      router.push("/notes");
+    } catch (error: any) {
+      toast.error(error.response.data.message);
+    }
+  };
 
-const handleLogin = async (values: FormValues) => {
-  try {
-    const { data } = await axios({
-      method: "post",
-      url: "http://localhost:8080/user/login",
-      data: values,
-    });
-    toast.success(data?.message);
-    window?.localStorage?.setItem("userToken",data.userToken)
-    router.push('/notes')
-  } catch (error: any) {
-    toast.error(error.response.data.message);
-  }
-};
+  const handleDemoLogin = () => {
+    handleLogin(demoLoginData);
+  };
 
-const handleDemoLogin = () => {
-  handleLogin(demoLoginData);
-};
-
-
-return (
-    <div className="container  mt-20  p-10 rounded-lg  glass md:w-[50%] xs:w-[75%] mx-auto">
-      <Toaster />
-
+  return (
+    <section className="container mt-20  py-6 sm:py-8 lg:py-12 mx-auto max-w-screen-2xl px-4 md:px-8">
+      <Toaster position="top-center"/>
+      <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">Login</h2>
       <Formik
         initialValues={initialValues}
         onSubmit={(values) => {
@@ -81,66 +78,71 @@ return (
         }}
         validationSchema={loginSchema}
       >
-        <Form className=" ">
-          <div className="mb-6">
-            <label className="block dark:text-white  mb-2">Email</label>
-            <Field
-              required
-              type="email"
-              name="email"
-              className="block rounded-lg border border-gray-300 
-        bg-gray-50  outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Enter your email address"
-            />
-            <ErrorMessage
-              name="email"
-              component="div"
-              className="text-red-500"
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block dark:text-white  mb-2">Password</label>
-            <Field
-              required
-              type="password"
-              name="password"
-              className="block rounded-lg border border-gray-300 
-        bg-gray-50  outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Enter your password"
-            />
-            <ErrorMessage
-              name="password"
-              component="div"
-              className="text-red-500"
-            />
-          </div>
+        <Form className="mx-auto max-w-lg rounded-lg border">
+          <div className="flex flex-col gap-4 p-4 md:p-8">
+            <div>
+              <label className="block dark:text-white  mb-2">Email</label>
+              <Field
+                required
+                type="email"
+                name="email"
+                className="block rounded-lg border border-gray-300 
+            bg-gray-50  outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter your email address"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
+            <div>
+              <label className="block dark:text-white  mb-2">Password</label>
+              <Field
+                required
+                type="password"
+                name="password"
+                className="block rounded-lg border border-gray-300 
+            bg-gray-50  outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter your password"
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
 
-          <p className="my-3 dark:text-white">
-            Don&apos;t have an account ?
-            <Link href="/signup" className="text-blue-400 underline">
-              Sign up
-            </Link>
-          </p>
-          <div className="flex justify-center"></div>
-          <LoginButton />
+            <LoginButton />
 
+            <div className="relative flex items-center justify-center">
+              <span className="absolute inset-x-0 h-px bg-gray-300"></span>
+              <span className="relative bg-white px-4 text-sm text-gray-400">
+                OR
+              </span>
+            </div>
+            <p
+              onClick={() => {
+                handleDemoLogin();
+              }}
+              className=" block cursor-pointer rounded-lg bg-indigo-800 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-700 focus-visible:ring active:bg-indigo-600 md:text-base"
+            >
+              Demo login
+            </p>
+          </div>
+          <div className="flex items-center justify-center bg-gray-100 p-4">
+            <p className="text-center text-sm text-gray-500">
+              Don&apos;t have an account?
+              <Link
+                href="/signup"
+                className="text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
+              >
+                Register
+              </Link>
+            </p>
+          </div>
         </Form>
       </Formik>
-      <div className="flex flex-col items-center justify-center">
-            <p className="flex items-end justify-center my-4">
-              <span className="w-6 h-1 bg-gray-600 inline-block mx-3 translate-y-[50%]"></span>
-              or
-              <span className="w-6 h-1 bg-gray-600 inline-block mx-3"></span>
-              </p>
-              <button
-                onClick={() => {
-                  handleDemoLogin()
-                }}
-                className=" my-5 p-2.5 bg-blue-500 rounded-2xl w-[50%] text-center inline-block text-white hover:bg-blue-600 focus:ring ring-blue-300"
-              >
-                Demo login
-              </button>
-          </div>
-    </div>
+    </section>
   );
 }
--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -46,7 +46,7 @@ const SignupButton = () => {
       <button
         disabled={!(formik.isValid && formik.dirty)}
         type="submit"
-        className="disabled:opacity-50 disabled:cursor-not-allowed p-2.5 bg-blue-500 rounded-full w-[50%] text-center inline-block text-white hover:bg-blue-600 focus:ring ring-blue-300"
+        className="w-full disabled:opacity-50 disabled:cursor-not-allowed block rounded-lg bg-indigo-800 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-700 focus-visible:ring active:bg-indigo-600 md:text-base"
       >
         Signup
       </button>
@@ -74,9 +74,9 @@ const handleSignup =async (values:FormValues)=>{
   }
 
   return (
-    <section className="container mx-auto">
+    <section className="container mt-20  py-6 sm:py-8 lg:py-12 mx-auto max-w-screen-2xl px-4 md:px-8">
       <Toaster position="top-center"/>
-      <div className="text-center text-xl font-semibold">Sign up</div>
+      <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">Signup</h2>
       <Formik
         initialValues={initialValues}
         onSubmit={(values) => {
@@ -84,107 +84,101 @@ const handleSignup =async (values:FormValues)=>{
         }}
         validationSchema={signupSchema}
       >
-        <Form className="mt-20 p-10 rounded-lg  glass sm:w-[50%] xs:w-[80%] mx-auto">
-          <div className="mb-6">
-            <label className="block dark:text-white  mb-3 ">
-              Name
-              </label>
-              <Field
-                required
-                type="text"
-                name="userName"
-                className="block rounded-lg border border-gray-300 
-              bg-gray-50  outline-none text-gray-900  ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Enter your name"
-              />
-              <ErrorMessage
-                name="userName"
-                component="div"
-                className="text-red-500"
-              />
-          </div>
-
-          <div className="mb-6">
-            <label className="block dark:text-white  mb-3">
-              Email
-              </label>
-              <Field
-                required
-                type="email"
-                name="email"
-                className="block rounded-lg border border-gray-300 
-              bg-gray-50  outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Enter your email address"
-              />
-              <ErrorMessage
-                name="email"
-                component="div"
-                className="text-red-500"
-              />
-            {/* {props.touched.email&&props.errors.email&&<p className="text-red-500">{errors.email}</p>} */}
-          </div>
-
-          <div className="mb-6">
-            <label className="block dark:text-white  mb-3">
-              Password
-              </label>
-              <Field
-                required
-                name="password"
-                type="password"
-                className="block rounded-lg border border-gray-300 
-    bg-gray-50  outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Enter your password"
-              />
-              <ErrorMessage
-                name="password"
-                component="div"
-                className="text-red-500"
-              />
-          </div>
-
-          <div className="mb-6">
-            <label className="block dark:text-white  mb-3">
-              Confirm password
-              </label>
-              <Field
-                required
-                name="cPassword"
-                type="password"
-                className="block rounded-lg border border-gray-300 
-              bg-gray-50  outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Enter your password"
-              />
-              <ErrorMessage
-                name="cPassword"
-                component="div"
-                className="text-red-500"
-              />
-
-          </div>
-          <p className="my-3 dark:text-white">
+          <Form className="mx-auto max-w-lg rounded-lg border">
+        <div className="flex flex-col gap-4 p-4 md:p-8">
+            <div >
+              <label className="block dark:text-white  mb-3 ">
+                Name
+                </label>
+                <Field
+                  required
+                  type="text"
+                  name="userName"
+                  className="block rounded-lg border border-gray-300 
+                bg-gray-50  outline-none text-gray-900  ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Enter your name"
+                />
+                <ErrorMessage
+                  name="userName"
+                  component="div"
+                  className="text-red-500"
+                />
+            </div>
+          
+            <div >
+              <label className="block dark:text-white  mb-3">
+                Email
+                </label>
+                <Field
+                  required
+                  type="email"
+                  name="email"
+                  className="block rounded-lg border border-gray-300 
+                bg-gray-50  outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Enter your email address"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-red-500"
+                />
+              {/* {props.touched.email&&props.errors.email&&<p className="text-red-500">{errors.email}</p>} */}
+            </div>
+          
+            <div >
+              <label className="block dark:text-white  mb-3">
+                Password
+                </label>
+                <Field
+                  required
+                  name="password"
+                  type="password"
+                  className="block rounded-lg border border-gray-300 
+          bg-gray-50  outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Enter your password"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-red-500"
+                />
+            </div>
+          
+            <div >
+              <label className="block dark:text-white  mb-3">
+                Confirm password
+                </label>
+                <Field
+                  required
+                  name="cPassword"
+                  type="password"
+                  className="block rounded-lg border border-gray-300 
+                bg-gray-50  outline-none text-gray-900 ring-blue-400 focus:border-blue-500 focus:ring-2 w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Enter your password"
+                />
+                <ErrorMessage
+                  name="cPassword"
+                  component="div"
+                  className="text-red-500"
+                />
+          
+            </div>
+            <SignupButton />
+            
+          
+        </div>
+        <div className="flex items-center justify-center bg-gray-100 p-4">
+            <p className="text-center text-sm text-gray-500">
             Do you have already an account ?
-            <Link href="/login" className="text-blue-400 underline">
-              Log in
-            </Link>
-          </p>
-
-          <SignupButton />
-        </Form>
+              <Link href="/login" className="text-blue-400 underline">
+                Log in
+              </Link>
+            </p>
+          </div>
+          </Form>
       </Formik>
     </section>
   );
 }
 
-{
-  /* <div className="flex justify-center">
 
- <button
-
-   type="submit"
-   className="disabled:opacity-50 disabled:cursor-not-allowed p-2.5 bg-blue-500 rounded-full w-[50%] text-center inline-block text-white hover:bg-blue-600 focus:ring ring-blue-300"
- >
-   Signup
- </button>
-</div> */
-}
